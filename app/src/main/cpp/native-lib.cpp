c933256599a513c4090885e7e51f137900bf17cc
hengfeng.zhuo <hengfeng.zhuo@rokid.com>
2019-02-16 13:14:54 +0800

    修改so文件

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -10,11 +10,7 @@ Java_com_rokid_rokidsecretdemo_MainActivity_getRokidSecret(JNIEnv *env, jobject 
     const char *sn = env->GetStringUTFChars(sn_, 0);
     const char *seed = env->GetStringUTFChars(seed_, 0);
 
-    //LOGD("Rokid =====>>snContent %s",sn); //601091847330038
-    //LOGD("Rokid =====>>seedContent %s",seed); //NZUNwyiLdxv0Ut2ItrHwVfLCAlu7zM
-
     char secretStr[64] = {0};
     getRokidSecret(sn , seed, secretStr);
-    //LOGD("Rokid =====>>seedContent %s",secretStr); //260EB4D4583B5BBA91DEF14150109A39
     return (*env).NewStringUTF(secretStr);
 }
